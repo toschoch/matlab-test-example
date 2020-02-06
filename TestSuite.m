85b539e99b3f3486de7880025e34f965588b865e
Tobias Schoch <tobias.schoch@vtxmail.ch>
2020-02-06 21:22:53 +0100

    test matlab code

---
+++ TestSuite.m
@@ -0,0 +1,18 @@
+classdef TestSuite < matlab.unittest.TestCase
+    % SolverTest tests solutions to the quadratic equation
+    % a*x^2 + b*x + c = 0
+    
+    methods (Test)
+        function testRealSolution(testCase)
+            actSolution = myfunc(1,-3);
+            expSolution = [2,1];
+            testCase.verifyEqual(actSolution,expSolution);
+        end
+        function testImaginarySolution(testCase)
+            actSolution = myfunc(1,2);
+            expSolution = [-1+3i, -1-3i];
+            testCase.verifyEqual(actSolution,expSolution);
+        end
+    end
+    
+end 

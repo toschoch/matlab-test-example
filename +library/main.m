689c00ccf65b8d609ee15ce1473728aba9240f63
Tobias Schoch <tobias.schoch@vtxmail.ch>
2020-02-12 19:59:28 +0100

    matlab example for commandline arguments and exitcode

---
+++ +library/main.m
@@ -0,0 +1,6 @@
+function exitcode = main(a)
+%MAIN Summary of this function goes here
+%   Detailed explanation goes here
+exitcode = str2num(a);
+end
+
--- +library/myfunc.m
+++ +library/myfunc.m
@@ -1,9 +1,14 @@
 function response = myfunc()
     %MYFUNCTEST Summary of this function goes here
     %   Detailed explanation goes here
+    fid = fopen('input.json');
+    raw = fread(fid,inf);
+    str = char(raw');
+    fclose(fid);
+    config = jsondecode(str);
+    disp(config)
     file = py.open('myimage.jpg', 'rb');
     bytes = file.read();
     file.close();
     response = py.requests.put('https://postman-echo.com/put',bytes);
 end
-
